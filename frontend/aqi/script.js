3a26d8bffb1402cb72cf54858287b201b20ff1d5
maxmohan117-star <maxmohan117@gmail.com>
2026-04-23 14:13:24 +0530

    Add files via upload

---
+++ frontend/aqi/script.js
@@ -0,0 +1,14 @@
+const ctx = document.getElementById('chart');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'],
+    datasets: [{
+      label: 'Delhi AQI',
+      data: [300,320,310,330,350,340,320],
+      borderColor: 'purple',
+      fill: false
+    }]
+  }
+});
